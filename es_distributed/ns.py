6f6962e8d872db115ba4b7ca9b33a4744c6b6eb0
Trevor Morton <mortontc@rose-hulman.edu>
2019-04-22 22:51:50 -0400

    Fixed novelty vector formatting bug

--- es_distributed/ns.py
+++ es_distributed/ns.py
@@ -19,7 +19,6 @@ def compute_novelty_vs_archive(archive, novelty_vector, k, normalize = False):
     print(novelty_vector)
     if normalize:
         for point in archive:
-            print(point)
             max_vector = np.maximum(point.astype(np.float), max_vector)
 
     max_vector += max_vector == 0
